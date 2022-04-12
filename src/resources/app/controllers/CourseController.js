ad75aecd4223a04a36472be0dee82cdb9e74bd74
dongvuvd7 <vuducdong2k@gmail.com>
2022-04-12 10:29:40 +0700

    change STT in table to ID, ObjectId in _id to number autoincrement, update multiple force-delete and restore in trash table

--- src/resources/app/controllers/CourseController.js
+++ src/resources/app/controllers/CourseController.js
@@ -1,8 +1,5 @@
 const Course = require("../models/Course");
-const {
-    multipleMongooseToObject,
-    mongooseToObject,
-} = require("../../../ulti/mongoose");
+const { multipleMongooseToObject, mongooseToObject } = require("../../../ulti/mongoose");
 
 class CourseController {
     // [GET] /courses/:slug
@@ -29,6 +26,9 @@ class CourseController {
 
         const formData = req.body;
         formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+
+        //Nếu không dùng thư viện auto increment id thì muốn tự động tăng _id thì phải chọc vào database để gọi ra id lớn nhất rồi +1
+
         const course = new Course(req.body);
         course
             .save()
@@ -39,9 +39,7 @@ class CourseController {
     // [GET] /courses/:id/edit
     edit(req, res, next) {
         Course.findById(req.params.id)
-            .then((course) =>
-                res.render("courses/edit", { course: mongooseToObject(course) })
-            )
+            .then((course) => res.render("courses/edit", { course: mongooseToObject(course) }))
             .catch(next);
     }
 
@@ -49,48 +47,62 @@ class CourseController {
     update(req, res, next) {
         // res.json(req.body);
 
-        Course.updateOne({_id: req.params.id}, req.body)
-        .then(() => res.redirect('/me/stored/courses'))
-        .catch(next);
+        Course.updateOne({ _id: req.params.id }, req.body)
+            .then(() => res.redirect("/me/stored/courses"))
+            .catch(next);
     }
 
     //[DELETE] /courses/:id
-    destroy(req, res, next){
-        Course.delete({_id: req.params.id})
-        .then(() => res.redirect('back'))
-        .catch(next);
+    destroy(req, res, next) {
+        Course.delete({ _id: req.params.id })
+            .then(() => res.redirect("back"))
+            .catch(next);
     }
 
     //[DELETE] /courses/:id/force
-    forceDestroy(req, res, next){
-        Course.deleteOne({_id: req.params.id})
-        .then(() => res.redirect('back'))
-        .catch(next);
+    forceDestroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect("back"))
+            .catch(next);
     }
 
     // [PATCH] /courses/:id/restore
     restore(req, res, next) {
-        Course.restore({_id: req.params.id})
-        .then(() => res.redirect('back'))
-        .catch(next);
+        Course.restore({ _id: req.params.id })
+            .then(() => res.redirect("back"))
+            .catch(next);
     }
 
     // [POST] /courses/handle-form-actions
     handleFormActions(req, res, next) {
-        switch(req.body.action){
-            case 'delete':
+        switch (req.body.action) {
+            case "delete":
                 // res.json(req.body.courseIds);
                 //delete array of courses id
                 Course.delete({ _id: { $in: req.body.courseIds } })
-                    .then(() => res.redirect('back'))
+                    .then(() => res.redirect("back"))
+                    .catch(next);
+                break;
+
+            case "force-delete":
+                // res.json(req.body.courseIds);
+                // delete array of courses id
+                Course.deleteMany({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect("back"))
+                    .catch(next);
+                break;
+
+            case "restore":
+                // res.json(req.body.courseIds);
+                Course.restore({ _id: { $in: req.body.courseIds } })
+                    .then(() => res.redirect("back"))
                     .catch(next);
                 break;
 
             default:
-                res.json({message: 'Action is invalid'})
+                res.json({ message: "Action is invalid" });
         }
     }
-
 }
 
 module.exports = new CourseController();
--- src/resources/app/controllers/MeController.js
+++ src/resources/app/controllers/MeController.js
@@ -6,7 +6,17 @@ class MeController {
     storedCourses(req, res, next) {
         // res.send("Khóa học của tôi đây");
 
-        Promise.all([Course.find({}), Course.countDocumentsDeleted({})])
+        // res.json(res.locals._sort);
+
+        let courseQuery = Course.find({});
+
+        if(req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            });
+        }
+
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
             .then(([courses, deletedCount]) => 
                 res.render("me/stored-courses", {
                     courses: multipleMongooseToObject(courses),
@@ -32,10 +42,18 @@ class MeController {
 
     // [GET] /me/trash/courses
     trashCourses(req, res, next) {
-        // res.send("Khóa học của tôi đây");
+        // res.send("Khóa học đã xóa của tôi đây");
+
+        let courseQuery = Course.findDeleted({});
+
+        if(req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type,
+            });
+        }
 
         //show all the courses that are in the trash
-        Course.findDeleted({})
+        courseQuery
             .then((courses) =>
                 res.render("me/trash-courses", {
                     courses: multipleMongooseToObject(courses),
